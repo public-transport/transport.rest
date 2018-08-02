7c27ab97daf37e096ab9bc309c25f7971d496d14
Jannis R <mail@jannisr.de>
2018-08-02 18:50:44 +0200

    post on 2.vbb.transport.rest being down
    see also derhuerst/vbb-rest#29

--- feed.js
+++ feed.js
@@ -1,6 +1,21 @@
 'use strict'
 
 module.exports = [{
+	id: '4',
+	title: '2.vbb.transport.rest unavailable for the time being, way forward',
+	url: '#',
+	date: '2018-08-01T20:20:00+0200',
+	body: `\
+As you might have noticed, [\`2.vbb.transport.rest\`](https://github.com/derhuerst/vbb-rest/blob/2/docs/index.md) has been down again for many days. This is because VBB has blocked the IP address of the API server, as well as those of [our load-balancing nodes we had set up in response](https://github.com/derhuerst/vbb-rest/issues/29#issuecomment-405111356). More background info can be found at [\`derhuerst/vbb-rest#29\`](https://github.com/derhuerst/vbb-rest/issues/29).
+
+**I won't run the \`2.vbb.transport.rest\` endpoint for the time being. If you want to use consume VBB data using JavaScript, use [\`vbb-hafas\`](https://github.com/derhuerst/vbb-hafas) (the lib behind the API) directly, otherwise host your own \`vbb-rest\` instance for now.**
+
+@juliuste and I have met recently with VBB. We were able to communicate:
+
+- our desire to keep this API running,
+- that the \`2.vbb.transport.rest\` is *no* responsible for the amount of traffic they've received with a \`hafas-client\` \`User-Agent\`,
+- that an open [GTFS RT](https://developers.google.com/transit/gtfs-realtime/reference/) feed would solve many issues for both parties.`
+}, {
 	id: '3',
 	title: 'old vbb.transport.rest now shut off',
 	url: '#',
